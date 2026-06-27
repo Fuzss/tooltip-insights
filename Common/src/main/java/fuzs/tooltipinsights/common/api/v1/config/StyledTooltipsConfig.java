8d0d9d0668e3d3d6baee05836ec4e38ba530503e
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-06-27 06:40:17 +0200

    fix generic tooltip lines option

--- Common/src/main/java/fuzs/tooltipinsights/common/api/v1/config/StyledTooltipsConfig.java
+++ Common/src/main/java/fuzs/tooltipinsights/common/api/v1/config/StyledTooltipsConfig.java
@@ -6,13 +6,11 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextColor;
 
-public class StyledTooltipsConfig<T extends TooltipComponentsConfig> implements ConfigCore {
+public abstract class StyledTooltipsConfig<T extends TooltipComponentsConfig> implements ConfigCore {
     @Config(description = "Add descriptions and other useful information to tooltips.")
     public TooltipDescriptionMode tooltipDescriptions = TooltipDescriptionMode.ALWAYS;
     @Config(description = "Add a note on how to show descriptions when they are hidden.")
     public boolean tooltipDescriptionsHint = true;
-    @Config
-    public final T tooltipLines;
     @Config(description = "Formatting for setting a text color and various styles for the description component.")
     final TextFormattingConfig descriptionFormatting = new TextFormattingConfig(TextColor.GRAY);
     @Config(description = "Apply a fixed string before every initial description line.")
@@ -21,13 +19,11 @@ public class StyledTooltipsConfig<T extends TooltipComponentsConfig> implements 
     public Style descriptionStyle;
     public Component descriptionDecorationComponent;
 
-    public StyledTooltipsConfig(T tooltipLines) {
-        this.tooltipLines = tooltipLines;
-    }
-
     @Override
     public void afterConfigReload() {
         this.descriptionStyle = this.descriptionFormatting.getStyle();
         this.descriptionDecorationComponent = Component.literal(this.descriptionDecoration);
     }
+
+    public abstract T tooltipLines();
 }
